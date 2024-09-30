7d58bac8fcc9f3a138f35e6c07a03991ef2e5485
seattleGr3y <raincityredteam@outlook.com>
2024-09-29 20:25:26 -0700

    update to .cmd to be run first time setup and by each scheduled task

--- ChildSafetyScreenTimeLimiter.cmd
+++ ChildSafetyScreenTimeLimiter.cmd
@@ -1,5 +1,19 @@
 @echo off
+set "breakTimeWarning="
+set breakTimeWarningPSparam=%breakTimeWarning%
+set "breakTimeStart="
+set breakTimeStartPSparam=%breakTimeStart%
 setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
-powershell -NoProfile -ExecutionPolicy bypass -command "& { %~dpn0.ps1 %* }"
+
+
+if "%breakTimeWarningPSparam%" goto command1
+: command1
+start PowerShell -ExecutionPolicy Bypass -File .\ChildSafetyScreenTimeLimiter_Setup.ps1
+start PowerShell -ExecutionPolicy Bypass -File .\%~dpn0.ps1 -breakTimeWarning
 exit /b
 
+if "%breakTimeStartPSparam%" goto command2
+: command2
+start PowerShell -ExecutionPolicy Bypass -File .\ChildSafetyScreenTimeLimiter_Setup.ps1
+start PowerShell -ExecutionPolicy Bypass -File .\%~dpn0.ps1 -breakTimeStart
+exit /b
